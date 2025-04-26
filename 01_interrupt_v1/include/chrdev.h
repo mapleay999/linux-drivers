9f55731f88e1f1d4667be7f87c060f735084a08f
Mapleay <757273877@qq.com>
2025-04-26 23:05:35 +0800

    主要更新：新增interrupt_v1版本，1. 使得2个按键中断与1个基于字符驱动的LED控制引脚共存；2. 都挂接在一个设备树节点内的硬件信息和都挂接在一个设备 struct device 上，且自定义了一个object 类型，把这三个pins统一起来，面向对象编程；3. 遇到了解析设备树节点信息时的键字符串细节问题 -gpio、-gpios 与 xxx_named_xxx 函数。2025年4月26日23:05:32

---
+++ 01_interrupt_v1/include/chrdev.h
@@ -0,0 +1,37 @@
+#ifndef __CHRDEV_H__
+#define __CHRDEV_H__
+
+#define DEVICE_NAME "mapleay-chrdev-device"
+#define CLASS_NAME  "mapleay-chrdev-class"
+#define MINOR_BASE  0     /* 次设备号起始编号为 0 */
+#define MINOR_COUNT 1     /* 次设备号的数量为 1   */
+#define BUF_SIZE    1024  /* 内核缓冲区大小       */
+
+#define LEDON  1  /* 硬件LED灯 */
+#define LEDOFF 0
+
+struct gpio_pin_t{
+	int gpio;
+	struct gpio_desc *gpiod;
+	int flag;
+	int irq;
+};
+
+/* 字符设备的自定义私有数据结构 */
+struct cdev_private_data_t {
+    char   *buffer;         /* 内核缓冲区 */
+    size_t buf_size;       /* 缓冲区大小: 写依据此变量  */
+    size_t data_len;       /* 当前数据长度：读依据此变量 */
+};
+
+typedef struct chrdev_object {
+    struct cdev   dev;
+    struct class  *dev_class;
+    struct device *dev_device;
+    struct cdev_private_data_t dev_data;
+    dev_t  dev_num;
+    struct gpio_pin_t *gpio_keys; /* 应用于按键中断实验 */
+	struct gpio_pin_t *gpio_leds; /* 应用于LED灯亮灭实验 */
+}chrdev_t;
+
+#endif
